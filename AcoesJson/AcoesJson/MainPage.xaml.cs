4a07473f057d7c2bb8087c92bb57b65d7c225957
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-05-23 22:21:32 -0300

    Adicionar arquivos de projeto.

---
+++ AcoesJson/AcoesJson/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace AcoesJson
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnPesquisar_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            string acao = edtAcao.Text;
+            var json = await client.GetStringAsync($"https://www.okanebox.com.br/api/acoes/hist/{acao}/20220515/20220523/");
+
+            List<acoes> acoes = JsonConvert.DeserializeObject<List<acoes>>(json);
+
+            lstAcoes.ItemsSource = acoes;
+        }
+    }
+}
---
+++ AcoesJson/AcoesJson/acoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcoesJson
+{
+    public class acoes
+    {
+        public DateTime DATPRG { get; set; }
+        public double PREABE { get; set; }
+        public double PREMAX { get; set; }
+        public double PREMIN { get; set; }
+        public double PREMED { get; set; }
+        public double PREULT { get; set; }
+        public int QUATOT { get; set; }
+        public double VOLTOT { get; set; }
+    }
+}
